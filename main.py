fecefc524f1ac92f3b20a034cb7033821749f83e
KTCH <kk.wetec@gmail.com>
2020-09-09 22:08:55 +0300

    calc

---
+++ main.py
@@ -0,0 +1,10 @@
+x = int(input('Введите первое число:'))
+y = int(input('Введите второе число:'))
+z=int(input('Введите третье число:'))
+print('plus:',(x+y+z))
+print('minus:',(x-y-z))
+print('divide:',(x/y/z))
+print('multiply:',(x*y*z))
+
+
+
